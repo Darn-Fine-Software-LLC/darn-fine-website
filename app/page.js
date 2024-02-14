4e339822ed6f40543e3c3f94597dcba11d367068
Alex White <alex.white@hey.com>
2024-02-14 14:03:29 -0500

    Updates

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
           Darn Fine Software
         </span>
         <h1 className="block font-sans text-xl text-center">
-          custom web & mobile development
+          Custom Web & Mobile Development
         </h1>
         <div className="mt-8">
           <a
@@ -28,9 +28,45 @@ export default function Home() {
           Based in Hilliard Ohio, Darn Fine Software works with other small to
           medium size companies to develop custom websites, mobile and desktop
           applications <span className="font-bold">from the ground up</span>. We
-          don't try to sell you on expensive cloud services, instead we work
-          with you to find the best, most cost-effective way to solve your
-          business needs.
+          don't try to sell you on expensive cloud services or reused templates,
+          instead we work with you to find the best, most cost-effective way to
+          solve your business needs.
+        </p>
+      </section>
+
+      <div className="w-full h-[1px] bg-gray-500 my-12 max-w-sm mx-auto w-full" />
+
+      <section className="">
+        <p className="font-sans leading-relaxed text-gray-700">
+          <span className="block mb-2 font-bold text-gray-800">
+            Our process revolves around you.
+          </span>{" "}
+          When you come to us with an idea, we sit down and get to know you,
+          your business and the needs your idea is solving. This process
+          includes brainstorming different approaches, outlining feasability and
+          definining your success criteria.
+        </p>
+        <p className="font-sans leading-relaxed text-gray-700 mt-8">
+          Once we're both happy with moving the project forward, Darn Fine
+          Software starts with design and usability. We interview potential
+          users, talk with stakeholders and begin to wireframe the user
+          interface. This step is critical, it's important to get the
+          fundamentals right and create something that not only solves your
+          business need, but exceeds your user's expectations.
+        </p>
+        <p className="font-sans leading-relaxed text-gray-700 mt-8">
+          After designs have been completed and signed off on, Darn Fine
+          Software begins the development phase. During this phase, we keep a
+          constant line of communication with you. You always know exactly what
+          we're doing, and what we have left. Our developers are experienced in
+          creating modern, battle-tested applications that use industry best
+          practices.
+        </p>
+        <p className="font-sans leading-relaxed text-gray-700 mt-8">
+          Once we're both happy with the developed solution, Darn Fine Software
+          will ensure you have access to all code, assets and infastructure.
+          Because we're local, Darn Fine Software is just a phone call away when
+          you need to make changes.
         </p>
       </section>
     </main>
