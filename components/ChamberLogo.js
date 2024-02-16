e3f79b95d0f21dab8a0f7f8539b8f07cbb80c43c
Alex White <alex.white@hey.com>
2024-02-16 11:38:24 -0500

    Changing to static image, this is a lot easier, lighter weight approach.

--- components/ChamberLogo.js
+++ components/ChamberLogo.js
@@ -1,24 +1,13 @@
 "use client";
 
-import Head from "next/head";
-import Script from "next/script";
+import Image from "next/image";
 
 export default function ChamberLogo() {
   return (
-    <div className="mt-4 w-full flex flex-row items-center justify-center">
-      <div className="w-40">
-        <div id="mni-membership-638436742933307795" />
+    <div className="flex flex-row items-center justify-center mt-8">
+      <div className="w-16">
+        <Image width={200} height={134} src="/hacc.jpg" />
       </div>
-      <Script
-        src="https://hilliardchamber.chambermaster.com/Content/Script/Member.js"
-        onLoad={() => {
-          new window.MNI.Widgets.Member("mni-membership-638436742933307795", {
-            member: 4658,
-            styleTemplate:
-              "#@id{text-align:center;position:relative}#@id .mn-widget-member-name{display: none}#@id .mn-widget-member-logo{max-width:100%}",
-          }).create();
-        }}
-      />
     </div>
   );
 }
