2db3ff4a82333e284f7acc5261344a85c4389d51
Alex White <alex.white@hey.com>
2024-02-16 11:30:16 -0500

    Testing different styling

--- components/ChamberLogo.js
+++ components/ChamberLogo.js
@@ -6,7 +6,7 @@ import Script from "next/script";
 export default function ChamberLogo() {
   return (
     <div className="mt-4 w-full flex flex-row items-center justify-center">
-      <div className="w-32">
+      <div className="w-40">
         <div id="mni-membership-638436742933307795" />
       </div>
       <Script
@@ -15,7 +15,7 @@ export default function ChamberLogo() {
           new window.MNI.Widgets.Member("mni-membership-638436742933307795", {
             member: 4658,
             styleTemplate:
-              "#@id{text-align:center;position:relative}#@id .mn-widget-member-name{font-weight:700}#@id .mn-widget-member-logo{max-width:100%}",
+              "#@id{text-align:center;position:relative}#@id .mn-widget-member-name{display: none}#@id .mn-widget-member-logo{max-width:100%}",
           }).create();
         }}
       />
