ca3f9249e50809d3ddb9207930be6160f969e78f
Alex White <alex.white@hey.com>
2024-02-16 11:39:18 -0500

    Add alt text

--- components/ChamberLogo.js
+++ components/ChamberLogo.js
@@ -6,7 +6,12 @@ export default function ChamberLogo() {
   return (
     <div className="flex flex-row items-center justify-center mt-8">
       <div className="w-16">
-        <Image width={200} height={134} src="/hacc.jpg" />
+        <Image
+          alt="Hilliard Chamber of Commerce Logo"
+          width={200}
+          height={134}
+          src="/hacc.jpg"
+        />
       </div>
     </div>
   );
