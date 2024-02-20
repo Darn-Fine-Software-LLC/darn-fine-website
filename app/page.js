900e3d90209c88c0cf30e397fd513829db0c261a
Alex White <alex.white@hey.com>
2024-02-20 12:54:33 -0500

    Add particle effect to header, refactor header to component

--- app/page.js
+++ app/page.js
@@ -1,48 +1,7 @@
+import Header from "@/components/Header";
 import InfoSection from "@/components/InfoSection";
 import Nav from "@/components/Nav";
 import ServicesSection from "@/components/ServicesSection";
-import Link from "next/link";
-
-const Header = () => {
-  return (
-    <header className="px-8 my-20 md:my-32 flex flex-col items-center justify-center">
-      <div className="max-w-4xl text-center">
-        <h1 className="text-2xl md:text-5xl font-bold tracking-tight text-white mb-6">
-          Custom web, mobile and desktop development in Hilliard, Ohio.
-        </h1>
-      </div>
-      <div className="max-w-xl text-center mb-8">
-        <p className="text-lg md:text-xl font-sans text-center tracking-tight text-slate-400">
-          We take your projects from idea to beautifully designed, modern
-          applications.
-        </p>
-      </div>
-      <a
-        href="https://forms.gle/NH2Bv81rhuzXdiDT8"
-        target="_blank"
-        className="rounded-lg px-6 py-3 text-sm font-bold hover:ring transition-all font-sans text-center text-slate-300 bg-slate-800 flex flex-row items-center gap-2 border-slate-700 border shadow-sm"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          ariaHidden="true"
-          data-slot="icon"
-          className="w-5 h-5"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
-          />
-        </svg>
-        Get a quote
-      </a>
-    </header>
-  );
-};
 
 export default function Home() {
   return (
